d2814ae15554d31c46b5540d743028245bb3d2b5
Adam Green <adagr407@student.liu.se>
2017-11-11 01:12:56 +0100

    simple movement

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Movement player;
+    private Quaternion fixedRotation;
+    private Vector3 distance;
+	// Use this for initialization
+	void Start () {
+        fixedRotation = transform.rotation;
+        player = FindObjectOfType<Movement>();
+        distance = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + distance;
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 1;
+    public float rotationSpeed = 1;
+    private CharacterController cc;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        if (Input.GetKey("s"))
+        {
+            x += -1;
+            z += -1;
+        }
+        else if (Input.GetKey("w"))
+        {
+            x += 1;
+            z += 1;
+        }
+        if (Input.GetKey("a"))
+        {
+            x += -1;
+            z += 1;
+        }
+        else if (Input.GetKey("d"))
+        {
+            x += 1;
+            z += -1;
+        }
+        cc.Move(new Vector3(Mathf.Clamp(x,-1,1), 0, Mathf.Clamp(z, -1, 1)) * speed * Time.deltaTime);
+
+        if (Input.GetKey("q"))
+            y = -1;
+        else if (Input.GetKey("e"))
+            y = 1;
+        transform.Rotate(0, y * rotationSpeed, 0);
+    }
+    
+}
