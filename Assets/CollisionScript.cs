42c712dbd24e0e46c130e8c72b1246f4c7b96611
Adam Green <adagr407@student.liu.se>
2017-11-11 12:38:18 +0100

    more movement stuff

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+    public bool canRotate = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        canRotate = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canRotate = true;
+    }
+}
