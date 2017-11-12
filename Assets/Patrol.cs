85c8b0d475857a17be575c98ab6badf51994b910
Adam Green <adagr407@student.liu.se>
2017-11-12 15:54:11 +0100

    stuff

---
+++ Assets/Patrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform pointA, pointB;
+    private Vector3 target;
+    public float speed = 1;
+    private Vector3 a, b;
+    // Use this for initialization
+    void Start()
+    {
+        a = pointA.position;
+        b = pointB.position;
+        target = a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance = target - transform.position;
+        transform.position += distance.normalized * Time.deltaTime * speed;
+        Quaternion q = new Quaternion();
+        q.eulerAngles = new Vector3(transform.rotation.x,(target==a)?0:180,transform.rotation.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, q, Time.deltaTime * 5);
+        if (distance.magnitude < 0.1)
+        {
+            if (target == a)
+                target = b;
+            else
+                target = a;
+        }
+    }
+}
